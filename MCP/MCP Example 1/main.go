f9cdf17964911776475356515d3799f72d8d1c6b
icobani <ibrahim@imaconsult.com>
2026-03-14 09:35:11 +0300

    `.gitignore` dosyası güncellendi ve MCP için yeni bir örnek proje eklendi: Kullanıcıya selam gönderme işlevi ekleyen bir MCP sunucusu.

---
+++ MCP/MCP Example 1/main.go
@@ -0,0 +1,45 @@
+/*
+	ICI Tech Teknoloji A.Ş.
+	User        : ICI
+	Name        : Ibrahim COBANI
+	Date        : 14.03.2026
+	Time        : 07:35
+	Notes       :
+
+
+*/
+
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+)
+
+type Input struct {
+	Name string `json:"name" jsonschema:"the name of the person to greet"`
+}
+
+type Output struct {
+	Greeting string `json:"greeting" jsonschema:"the greeting to tell to the user"`
+}
+
+func SayHi(ctx context.Context, req *mcp.CallToolRequest, input Input) (
+	*mcp.CallToolResult,
+	Output,
+	error,
+) {
+	return nil, Output{Greeting: "Hi " + input.Name}, nil
+}
+
+func main() {
+	// Create a server with a single tool.
+	server := mcp.NewServer(&mcp.Implementation{Name: "greeter", Version: "v1.0.0"}, nil)
+	mcp.AddTool(server, &mcp.Tool{Name: "greet", Description: "say hi"}, SayHi)
+	// Run the server over stdin/stdout, until the client disconnects.
+	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
+		log.Fatal(err)
+	}
+}
